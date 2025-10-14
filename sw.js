f6866d563f532e27b621b563a1105f574e7b3d32
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-14 19:57:25 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('acw-v1').then(c=>c.addAll(['./','./index.html','./manifest.json','./acw-icon.png'])));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => { self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });

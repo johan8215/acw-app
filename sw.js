7700cb4235c42493542958ae0dbd9b3bc26c02e1
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-14 18:38:51 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { self.clients.claim(); });
-// Placeholder for future offline caching if needed.

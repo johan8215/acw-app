0e3912ddea16dabac36077d8a2be791627781954
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-15 16:12:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ACW_CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbzmUDG6V8Z1fIUzwFPXWD8Ws0WcNHwh7vNhMPANX7rNmY-5AMqzgUkxtrsM4YclNiOl/exec",
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbyOhI4-ajJlXSQ-eIdWV6E7TPSpI6HJGNxovSSQPWfoCoII_6sfQm8BccXquhcwStgz/exec",
   GOOGLE_CLIENT_ID: "349643299522-g40ihht05th3cthd0d57pbpe36tkiuoj.apps.googleusercontent.com"
 };

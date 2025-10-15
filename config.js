b82c83102bc57f51b765d81d028d8628719ab167
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-15 10:45:14 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// ACW App Config
-window.ACW_CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbwiCW4AUuUvGy_1zwMnMB0cj-JXB4pU5a4_km4MwC0p2HPsEroF6Tm0wOCl-dQXrOWO/exec",
-  GOOGLE_CLIENT_ID: "349643299522-g40ihht05th3cthd0d57pbpe36tkiuoj.apps.googleusercontent.com"
-};

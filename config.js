dada8288bdd077748da0dcad7346f717457afc92
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-15 22:23:59 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ACW frontend config
+window.ACW_CONFIG = {
+  BACKEND_URL: "https://script.google.com/macros/s/AKfycbwnqRSSSET2HwDt4ItUOwAyx0pSvP1hqoygeoiJ5jIyWQ7A1Zrd8jl8LIV4IWDucYCp/exec",
+  GOOGLE_CLIENT_ID: "349643299522-g40ihht05th3cthd0d57pbpe36tkiuoj.apps.googleusercontent.com"
+};

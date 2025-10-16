c3021cd09ad0fa646d675c0f31f29af8220549ec
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-15 21:39:34 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Allston Car Wash – App Configuration (v3.7.1)
-window.ACW_CONFIG = {
-  BACKEND_URL: "https://script.google.com/macros/s/AKfycbyOhI4-ajJlXSQ-eIdWV6E7TPSpI6HJGNxovSSQPWfoCoII_6sfQm8BccXquhcwStgz/exec",
-  GOOGLE_CLIENT_ID: "349643299522-g40ihht05th3cthd0d57pbpe36tkiuoj.apps.googleusercontent.com"
-};

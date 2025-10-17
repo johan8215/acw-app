54e27254c4b4b4177ce8201c7c392450b17b607d
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-16 22:40:54 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Configuration for ACW-App Connected Lite
-const CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbzEj2f739VD2nvYMfaJ-ENMIA1uA-gcGlJhlUWx9DPqldCPpPPjP-omeE4THVtB2oS6/exec",
-  VERSION: "3.8 Connected Lite",
-  AUTHOR: "Johan A. Giraldo | jag15.com"
-};

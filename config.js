31324c24ae2f9205099159794dde74d1baeb8fb8
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-16 22:30:16 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Configuration for ACW-App Connected Lite
 const CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbwXnB6kBsbgbpGAo0g6YyWnmqr0LjBmNmDh0zZxnQVCSHS7Ms1rd8h2YwSG_GyGC-9R/exec",
+  BASE_URL: "https://script.google.com/macros/s/AKfycbzEj2f739VD2nvYMfaJ-ENMIA1uA-gcGlJhlUWx9DPqldCPpPPjP-omeE4THVtB2oS6/exec",
   VERSION: "3.8 Connected Lite",
   AUTHOR: "Johan A. Giraldo | jag15.com"
 };

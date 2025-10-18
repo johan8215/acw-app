1b3ca472d751d6482c96f19e74d88a2a7a489e17
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-17 21:36:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
    =========================================================== */
 
 const CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbz5Bi8dAtG5ahRp_mWwXm-_JTxC0QHA6YoBzLWAOJ7HLK8l3zT1QKPanSpJCDHV_5m3/exec",
+  BASE_URL: "https://script.google.com/macros/s/AKfycbyFYVoFzoPwHHq9M0j6ifnqnT-Zmezsp-XCstMg_vVL-BP2lnhWoImtsFbQFkdD8XoA/exec",
   APP_NAME: "Allston Car Wash",
   VERSION: "3.8.Connected.Lite",
 };

ad91c0da4bba8e78782cc39c256d5bd197cc7af4
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-17 22:43:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
+/* ===========================================================
+   ACW-App v3.8 Connected Lite
+   Author: Johan A. Giraldo (JG) | Allston Car Wash
+   =========================================================== */
+
 const CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbyxHF4rBFvCNqCoFi31lpdaBzjvUU5_00Cmv42rFiaRniGutv8W5QUToD7ip-LcGQpP/exec",
+  BASE_URL: "https://script.google.com/macros/s/AKfycbz5Bi8dAtG5ahRp_mWwXm-_JTxC0QHA6YoBzLWAOJ7HLK8l3zT1QKPanSpJCDHV_5m3/exec",
   APP_NAME: "Allston Car Wash",
   VERSION: "3.8.Connected.Lite",
 };

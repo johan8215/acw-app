74f5a650da3dc64fbc4b4c44a4f1d0ea7994d26a
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-19 18:37:02 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 /* ===========================================================
-   ACW-App v3.8 Connected Lite
+   ACW-App v4.0 Red Glass Edition
    Author: Johan A. Giraldo (JG) | Allston Car Wash
    =========================================================== */
 
 const CONFIG = {
   BASE_URL: "https://script.google.com/macros/s/AKfycbyvKBaKxvAhoudDIXjUcmIef31ZTyvhujLRxzOoyQBneGpnpcnJkYpyzvXS3SCve4Z6/exec",
+  API_KEY: "4859356", // ✅ CallMeBot API key
   APP_NAME: "Allston Car Wash",
-  VERSION: "3.8.Connected.Lite",
+  VERSION: "4.0.Red.Glass"
 };

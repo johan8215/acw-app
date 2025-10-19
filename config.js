5fa0fe0f7a645d7afeb154adf9060a947df08c56
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-18 21:23:11 -0400

    Restore ACW-App v3.8 (Stable)

---
+++ config.js
@@ -0,0 +1,10 @@
+/* ===========================================================
+   ACW-App v3.8 Connected Lite
+   Author: Johan A. Giraldo (JG) | Allston Car Wash
+   =========================================================== */
+
+const CONFIG = {
+  BASE_URL: "https://script.google.com/macros/s/AKfycbyvKBaKxvAhoudDIXjUcmIef31ZTyvhujLRxzOoyQBneGpnpcnJkYpyzvXS3SCve4Z6/exec",
+  APP_NAME: "Allston Car Wash",
+  VERSION: "3.8.Connected.Lite",
+};
